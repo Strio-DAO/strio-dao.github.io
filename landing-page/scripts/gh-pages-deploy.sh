0c2b123fd3dbd3cd2c1affbb4f0ea3f84d355f83
cryptosremac <cryptosremac@gmail.com>
2021-12-11 19:35:48 +0100

    wip deploy

---
+++ landing-page/scripts/gh-pages-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+line="\n#######################################################################\n";
+build_folder=dist
+
+git branch -D gh-pages
+
+git checkout --orphan gh-pages
+
+npm run build
+
+git --work-tree $build_folder add --all
+git --work-tree $build_folder commit -m gh-pages
+
+git push origin HEAD:gh-pages --force
+
+rm -f $build_folder
+
+git checkout -f master
+git branch -D gh-pages
